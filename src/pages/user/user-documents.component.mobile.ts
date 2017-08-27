347a77e121bfb2b916da47fa8d7618d402c66c3c
Santiago Gonzalez <hernangt12re3@gmail.com>
2017-08-27 17:46:13 -0500

    Added observable zone

--- src/pages/user/user-documents.component.mobile.ts
+++ src/pages/user/user-documents.component.mobile.ts
@@ -6,7 +6,7 @@ declare var Meteor;
 declare var _;
 
 import { NavController, ViewController, LoadingController, ModalController, NavParams} from 'ionic-angular';
-import { Subscription } from "rxjs";
+import { Observable, Subscription } from "rxjs";
 import { MeteorObservable } from "meteor-rxjs";
 
 import { UploadDocumentsMobileComponent } from '../user/upload-documents.component.mobile';
@@ -32,17 +32,20 @@ export class UserDocumentsMobileComponent implements OnInit, OnDestroy {
 	imagesSub: Subscription;
 	images: Image[];
 
+	imagesObs: Observable<Image[]>;
+
 	constructor(private navCtrl: NavController, private viewCtrl: ViewController, private loadingCtrl: LoadingController, private modalCtrl: ModalController, private navParams: NavParams) {
 
 		this.isDriver = Meteor.user()['personData'].isDriver;
-    this.dniStatus = this.licenceStatus = this.registerStatus = 0;
-    this.uploadDocuments = UploadDocumentsMobileComponent;
+	    this.dniStatus = this.licenceStatus = this.registerStatus = 0;
+	    this.uploadDocuments = UploadDocumentsMobileComponent;
 	}
 
 	ngOnInit() {
+
 		MeteorObservable.autorun().subscribe(() => {
-      this.user = Meteor.user();
-    });
+  			this.user = Meteor.user();
+    	});
 
 		if(this.imagesSub){
 			this.imagesSub.unsubscribe();
@@ -50,27 +53,33 @@ export class UserDocumentsMobileComponent implements OnInit, OnDestroy {
 
 		this.imagesSub = MeteorObservable.subscribe('images', this.user._id).subscribe(() => {
 
-			let query = Images.find({doc_type: {$in: [1, 2, 3]}})
-      let images = query.fetch();
+			let query = Images.find({doc_type: {$in: [1, 2, 3]}});
 
-			let dniDoc = _.filter(images, function(image){
+			this.imagesObs = query.zone();
+
+      		let images = query.fetch();
+
+			let dniDoc = _.filter(images, function(image) {
 				return image.doc_type == 1;
 			});
-			if(dniDoc.length > 0){
+
+			if(dniDoc.length > 0) {
 				this.dniStatus = dniDoc[dniDoc.length -1].processed == undefined ? 1 : ( dniDoc[dniDoc.length -1 ].processed ? 2 : 0 );
 			}
 
-			let licenceDoc = _.filter(images, function(image){
+			let licenceDoc = _.filter(images, function(image) {
 				return image.doc_type == 2;
 			});
-			if(licenceDoc.length > 0){
+
+			if(licenceDoc.length > 0) {
 				this.licenceStatus = licenceDoc[licenceDoc.length - 1].processed == undefined ? 1 : ( licenceDoc[licenceDoc.length - 1].processed ? 2 : 0 );
 			}
 
-			let registerDoc = _.filter(images, function(image){
+			let registerDoc = _.filter(images, function(image) {
 				return image.doc_type == 3;
 			});
-			if(registerDoc.length > 0){
+			
+			if(registerDoc.length > 0) {
 				this.registerStatus = registerDoc[registerDoc.length - 1].processed == undefined ? 1 : ( registerDoc[registerDoc.length - 1].processed ? 2 : 0 );
 			}
 		});
@@ -80,12 +89,12 @@ export class UserDocumentsMobileComponent implements OnInit, OnDestroy {
 		this.imagesSub.unsubscribe();
 	}
 
-  openPage(page: Component, params: any) {
-    let modal = this.modalCtrl.create(page, params);
-    modal.present();
-  }
+	openPage(page: Component, params: any) {
+		let modal = this.modalCtrl.create(page, params);
+		modal.present();
+	}
 
-  dismiss() {
+	dismiss() {
 		this.viewCtrl.dismiss();
 	}
 }
