4d575ed3394578a88e29ccd2aeae70211e8b2d6f
Oscar Malgiaritta <malgia@hotmail.com>
2017-12-05 10:28:40 -0500

    Removed comments of apn on push.ts

--- api/server/imports/push.ts
+++ api/server/imports/push.ts
@@ -6,16 +6,16 @@ import { Notification, NotificationBody } from '../../both/models/notification.m
 Push.debug = true;
 
 Push.Configure({
-  // apn: {
-  //   certData: Assets.getText('push_certs/SimpleRide-dev.pem'), // development
-  //   keyData: Assets.getText('push_certs/SimpleRideKey-dev.pem'), // development
-  //   // certData: Assets.getText('push_certs/SimpleRide-prod.pem'), // production
-  //   // keyData: Assets.getText('push_certs/SimpleRideKey-prod.pem'), // production
-  //   passphrase: 'simpleride',
-  //   production: false,
-  //   gateway: 'gateway.sandbox.push.apple.com' // development
-  //   // gateway: 'gateway.push.apple.com' // production
-  // },
+  apn: {
+    certData: Assets.getText('push_certs/SimpleRide-dev.pem'), // development
+    keyData: Assets.getText('push_certs/SimpleRideKey-dev.pem'), // development
+    // certData: Assets.getText('push_certs/SimpleRide-prod.pem'), // production
+    // keyData: Assets.getText('push_certs/SimpleRideKey-prod.pem'), // production
+    passphrase: 'simpleride',
+    production: false,
+    gateway: 'gateway.sandbox.push.apple.com' // development
+    // gateway: 'gateway.push.apple.com' // production
+  },
   gcm: {
     apiKey: 'AAAAFdnDuNc:APA91bEgzEvj-4MwU9xvEoyYSt1bmzzMD-naR-02ZPwlQvUWrpxCIHIKbDtsMU1WytYaLcagYI8qldvpAynpiL47hBYEnZMfPDQHsd0u-bK1QgaEGlBYj3EFzMQQgSImFL5RwwS2TQe3',
   }
